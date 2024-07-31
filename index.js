5ebd2544a15f93bc5ae1b5841829e77c66112344
Adam Tak <adam.tak2112@gmail.com>
2024-07-31 09:27:47 -0600

    Setup post route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const dns = require('node:dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,9 +18,33 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let index = 1;
+
+const urlArr = [];
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+
+
+app.post('/api/shorturl', function(req, res){
+  if (dns.lookup(req.body.url, (err, addresses) => {
+    if (err){ console.error(err); }
+    else { console.log(addresses) }
+  })){
+    urlArr.push(req.body.url)
+    res.json({
+      original_url: `${req.body.url}`,
+
+    })
+  } else {
+    res.json({
+      error: 'invalid url'
+    });
+  }
+  
 });
 
 app.listen(port, function() {
